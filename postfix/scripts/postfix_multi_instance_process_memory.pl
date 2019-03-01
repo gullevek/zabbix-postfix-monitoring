2d8161e807bca985c23c4ae9a17ddcced12ce492
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2019-03-01 18:56:33 +0900

    Zabbix Postfix mult instance process and memory script update
    Auto check for master controller and set override spool path.
    Auto set empty values to 0 for Zabbix processing
    Update user parameter file with new settings

--- postfix/scripts/postfix_multi_instance_process_memory.pl
+++ postfix/scripts/postfix_multi_instance_process_memory.pl
@@ -22,6 +22,8 @@ my $queue_dir;
 my $pid;
 my $processes;
 my @process_list = ();
+# define list of data that needs to be there and is init as 0 if not set
+my @default_process_list = ('anvil', 'bounce', 'cleanup', 'flush', 'local', 'pickup', 'qmgr', 'scache', 'showq', 'smtpd', 'smtp', 'tlsmgr', 'trivial-rewrite', 'verify', 'virtual');
 # general variables
 my $debug = 0;
 
@@ -48,7 +50,16 @@ if (!$syslog_name) {
 # so we need to get the spool folder
 $queue_dir = `/usr/sbin/postmulti -i $syslog_name -x postconf -xh queue_directory 2>/dev/null`;
 chomp $queue_dir;
-	# if we have a queue dir
+# if we don't have a queue dir, check if this syslog name matches the master name, if not error on not found
+if (!$queue_dir) {
+	my $_syslog_name = `/usr/sbin/postmulti -i - -x postconf -xh syslog_name`;
+	chomp $_syslog_name;
+	if ($_syslog_name && $_syslog_name eq $syslog_name) {
+		$queue_dir = `/usr/sbin/postmulti -i - -x postconf -xh queue_directory 2>/dev/null`;
+		chomp $queue_dir;
+	}
+}
+# if we have a queue dir
 if ($queue_dir) {
 	# get the master pid & remove all spaced
 	# NOTE needs sudo entry for cat -> pid/master.pid
@@ -65,16 +76,33 @@ if ($queue_dir) {
 	}
 	# get memory (in KB)
 	# ps -o pid:1=,comm=,vsz:1= --ppid 13596
-	if (!$debug) {
-		print encode_json($processes);
-	} else {
-		foreach my $_key (sort keys %$processes) {
-			print "[$syslog_name] ".$_key."[num]: ".$processes->{$_key}->{'num'}."\n";
+	open(FH, 'ps -o pid:1=,comm=,vsz:1= --ppid '.$pid.'|') || die("Can't open ps: ".$!."\n");
+	while (<FH>) {
+		chomp;
+		my ($_pid, $_process, $_mem) = split(/\s+/, $_);
+		$processes->{$_process}->{'mem'} += $_mem;
+	}
+	close(FH);
+	# loop through default and fill missing with 0
+	foreach my $_process (@default_process_list) {
+		if (!$processes->{$_process}->{'num'}) {
+			$processes->{$_process}->{'num'} = 0;
+			$processes->{$_process}->{'mem'} = 0;
 		}
+	}
+	# ouput debug print or json encode
+	if ($debug) {
+		foreach my $_key (sort keys %$processes) {
+			foreach my $_sub ('num', 'mem') {
+				print "[$syslog_name] ".$_key."[".$_sub."]: ".$processes->{$_key}->{$_sub}."\n";
+			}
+		}
+	} else {
+		print encode_json($processes);
 	}
 } else {
 	# no queue dir, no syslog name, just skip with empty
-	print '{"error":"queue directory not found: '.$queue_dir.'"}';
+	print '{"error":"queue directory not found: '.$syslog_name.'"}';
 }
 
 # __END__
